f78811fe39e799a7437b76b5e37100dd3d7b28cd
Alexandra Ryzvanovich <alexandraryzvanovich@gmail.com>
2020-02-06 14:51:04 +0300

    get commands

--- src/main/java/com/epam/webapp/command/CommandFactory.java
+++ src/main/java/com/epam/webapp/command/CommandFactory.java
@@ -16,13 +16,13 @@ public class CommandFactory {
             case "buySubscription": return new BuySubscriptionCommand(new OrderService(new DaoHelperFactory()));
             case "getAvailableSubscriptions": return new GetAvailableSubscriptionsCommand(new SubscriptionService(new DaoHelperFactory()));
             case "getOrders": return new GetOrdersCommand(new OrderService(new DaoHelperFactory()));
+            case "getInterns": return new GetTrainerInternsCommand(new UserService(new DaoHelperFactory()));
 
 
             case "getTrainingProgram": return new GetTrainingProgramCommand(new TrainingProgramService(new DaoHelperFactory()));
             case "getAllUsersCommand": return new GetAllUsersCommand(new UserService(new DaoHelperFactory()));
             case "getReviews": return new GetReviewsCommand(new ReviewService(new DaoHelperFactory()));
             case "getClients": return new ShowPageCommand("/WEB-INF/views/clients.jsp");
-            case "getInterns": return new GetTrainerInternsCommand(new UserService(new DaoHelperFactory()));
 
 
             default: throw new IllegalArgumentException("unknown command " + command);
