a7def431bb331592482914727d6df0f908c847f8
Alexandra Ryzvanovich <Alexandraryzvanovich@gmail.com>
2020-02-08 01:22:13 +0300

    refactor

---
+++ src/main/java/com/epam/webapp/filter/SessionFilter.java
@@ -0,0 +1,52 @@
+package com.epam.webapp.filter;
+
+import com.epam.webapp.command.CommandResult;
+import com.epam.webapp.command.ShowPageCommand;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class SessionFilter implements Filter {
+    private static final String ID_ATTRIBUTE = "id";
+    private static final String LOGIN_JSP_PAGE = "/WEB-INF/views/common/login.jsp";
+    private ServletContext context;
+    private ArrayList<String> urlList;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        String urls = filterConfig.getInitParameter("avoid-urls");
+        StringTokenizer token = new StringTokenizer(urls, ",");
+        urlList = new ArrayList<>();
+        while (token.hasMoreTokens()) {
+            urlList.add(token.nextToken());
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        String url = request.getServletPath();
+        boolean allowedRequest = false;
+        if (urlList.contains(url)) {
+            allowedRequest = true;
+        }
+
+        if (!allowedRequest) {
+            HttpSession session = request.getSession(false);
+            if (null == session) {
+                CommandResult.redirect(LOGIN_JSP_PAGE);
+            }
+        }
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
