a0af19ea494537c61c8a532e2b01f9f90938bba5
Alexandra Ryzvanovich <Alexandraryzvanovich@gmail.com>
2020-02-07 19:51:11 +0300

    refactor

--- src/main/java/com/epam/webapp/command/client/BuySubscriptionCommand.java
+++ src/main/java/com/epam/webapp/command/client/BuySubscriptionCommand.java
@@ -4,14 +4,17 @@ import com.epam.webapp.command.Command;
 import com.epam.webapp.command.CommandResult;
 import com.epam.webapp.entity.Order;
 import com.epam.webapp.entity.OrderStatus;
+import com.epam.webapp.entity.Subscription;
 import com.epam.webapp.exception.CommandException;
 import com.epam.webapp.exception.ServiceException;
 import com.epam.webapp.service.OrderService;
+import com.epam.webapp.service.SubscriptionService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Optional;
 
 public class BuySubscriptionCommand implements Command {
     private static final String ID_ATTRIBUTE = "id";
@@ -22,10 +25,12 @@ public class BuySubscriptionCommand implements Command {
     private static final String SUCCESS_MSG_ATTRIBUTE = "success";
     private static final String SUCCESS_MESSAGE = "You have bought subscription successfully";
 
-    private OrderService service;
+    private OrderService orderService;
+    private SubscriptionService subscriptionService;
 
-    public BuySubscriptionCommand(OrderService service) {
-        this.service = service;
+    public BuySubscriptionCommand(OrderService orderService, SubscriptionService subscriptionService) {
+        this.orderService = orderService;
+        this.subscriptionService = subscriptionService;
     }
 
     @Override
@@ -35,19 +40,21 @@ public class BuySubscriptionCommand implements Command {
         String stringId = idAttribute.toString();
         Long userId = Long.parseLong(stringId);
         Date paidDate = new Date();
-        String amountParam = request.getParameter(AMOUNT_PARAMETER);
-        BigDecimal amount = new BigDecimal(amountParam);
         String statusParam = request.getParameter(STATUS_PARAMETER);
         OrderStatus status = OrderStatus.valueOf(statusParam);
         String subscriptionIdParam = request.getParameter(SUBSCRIPTION_ID_PARAMETER);
         Long subscriptionId = Long.parseLong(subscriptionIdParam);
-        Order order = new Order(userId, paidDate, amount, status, subscriptionId);
         try {
-            service.addOrder(order);
-            request.setAttribute(SUCCESS_MSG_ATTRIBUTE, SUCCESS_MESSAGE);
-            return CommandResult.redirect(REVIEWS_PAGE);
+            Optional<Subscription> subscription = subscriptionService.getSubscriptionById(subscriptionId);
+            if(subscription.isPresent()) {
+                BigDecimal amount = subscription.get().getPrice();
+                        Order order = new Order(userId, paidDate, amount, status, subscriptionId);
+                orderService.addOrder(order);
+                request.setAttribute(SUCCESS_MSG_ATTRIBUTE, SUCCESS_MESSAGE);
+            }
         } catch (ServiceException e) {
             throw new CommandException("Error occurred while executing command", e.getCause());
         }
+        return CommandResult.redirect(REVIEWS_PAGE);
     }
 }
--- src/main/java/com/epam/webapp/service/SubscriptionService.java
+++ src/main/java/com/epam/webapp/service/SubscriptionService.java
@@ -8,6 +8,7 @@ import com.epam.webapp.exception.DaoException;
 import com.epam.webapp.exception.ServiceException;
 
 import java.util.List;
+import java.util.Optional;
 
 public class SubscriptionService {
     private DaoHelperFactory daoHelperFactory;
@@ -20,6 +21,15 @@ public class SubscriptionService {
         try (DaoHelper factory = daoHelperFactory.create()) {
             AbstractDao<Subscription> dao = (AbstractDao<Subscription>) factory.createSubscriptionDao();
             return dao.getAll();
+        } catch (DaoException e) {
+            throw new ServiceException(e.getCause());
+        }
+    }
+
+    public Optional<Subscription> getSubscriptionById(Long id) throws ServiceException {
+        try (DaoHelper factory = daoHelperFactory.create()) {
+            AbstractDao<Subscription> dao = (AbstractDao<Subscription>) factory.createSubscriptionDao();
+            return dao.getById(id);
         } catch (DaoException e) {
             throw new ServiceException(e.getCause());
         }
