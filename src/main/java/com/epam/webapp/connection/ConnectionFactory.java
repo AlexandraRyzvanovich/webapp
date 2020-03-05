19bc420910ead398cd8aaa1f1bc7d74a67125a61
Alexandra Ryzvanovich <Alexandraryzvanovich@gmail.com>
2020-03-05 19:19:42 +0300

    refactoring

--- src/main/java/com/epam/webapp/connection/ConnectionFactory.java
+++ src/main/java/com/epam/webapp/connection/ConnectionFactory.java
@@ -17,21 +17,33 @@ public class ConnectionFactory {
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
     private static final String PROPERTIES_FILE_PATH = "src/main/webapp/jdbc.properties";
+    private static String url;
+    private static String username;
+    private static String password;
 
-    public static ProxyConnection create(ConnectionPool connectionPool) {
+    public ConnectionFactory() {
         Path path = Paths.get(PROPERTIES_FILE_PATH);
         InputStream file;
         try {
             file = Files.newInputStream(path);
             Properties properties = new Properties();
             properties.load(file);
-            String url = (String) properties.get(URL);
-            String username = (String) properties.get(USERNAME);
-            String password = (String) properties.get(PASSWORD);
-            Connection connection = DriverManager.getConnection(url, username, password);
-            return new ProxyConnection(connection, connectionPool);
-        } catch (IOException | SQLException e) {
-            throw new ConnectionException("Get connection failed");
+            url = (String) properties.get(URL);
+            username = (String) properties.get(USERNAME);
+            password = (String) properties.get(PASSWORD);
+        } catch (IOException e) {
+            throw new ConnectionException(e.getMessage());
         }
+    }
+
+    public static ProxyConnection create(ConnectionPool connectionPool) {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new ConnectionException(e.getMessage());
+        }
+        return new ProxyConnection(connection, connectionPool);
     }
 }
