6267778e9371e69a92f286925bd483271e255227
Alexandra Ryzvanovich <Alexandraryzvanovich@gmail.com>
2020-03-07 20:08:03 +0300

    Refactoring

--- src/main/java/com/epam/webapp/command/trainer/GetInternProgramCommand.java
+++ src/main/java/com/epam/webapp/command/trainer/GetInternProgramCommand.java
@@ -18,9 +18,7 @@ public class GetInternProgramCommand implements Command {
     private static final String ID_PARAMETER = "id";
     private static final String CURRENT_PROGRAM_ATTRIBUTE = "currentProgram";
     private static final String PROGRAM_JSP_PAGE = "/WEB-INF/views/internCard.jsp";
-    private static final String PROGRAM_STATUS_LIST_ATTRIBUTE = "programStatusList";
     private static final String EXERCISES_LIST_ATTRIBUTE = "exercises";
-    private static final String DIET_LIST_ATTRIBUTE = "dietList";
 
     private final ProgramService programService;
 
@@ -35,11 +33,12 @@ public class GetInternProgramCommand implements Command {
     public CommandResult execute(HttpServletRequest request) throws CommandException {
         String userIdParameter = request.getParameter(ID_PARAMETER);
         Long id = Long.parseLong(userIdParameter);
+
         try {
             Map<Program, List<TrainingProgramCard>> currentProgram = programService.getFullCurrentTrainingProgramInfo(id);
-                List<Exercise> allExercises = exerciseService.getAllExercises();
-                request.setAttribute(EXERCISES_LIST_ATTRIBUTE, allExercises);
-                request.setAttribute(CURRENT_PROGRAM_ATTRIBUTE, currentProgram);
+            List<Exercise> allExercises = exerciseService.getAllExercises();
+            request.setAttribute(EXERCISES_LIST_ATTRIBUTE, allExercises);
+            request.setAttribute(CURRENT_PROGRAM_ATTRIBUTE, currentProgram);
             return CommandResult.forward(PROGRAM_JSP_PAGE);
         } catch (ServiceException e) {
             throw new CommandException("Error occurred while executing command", e.getCause());
